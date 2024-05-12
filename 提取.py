bf7d4053986ccb392b0d3d56cd3975c8c5dbf0af
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-12 18:02:45 +0800

    Update 提取.py

--- 提取.py
+++ 提取.py
@@ -46,7 +46,7 @@ for file_path in file_paths:
         file_contents.append(content)
 
 # 写入合并后的文件
-with open("iptv_list.txt", "w", encoding="utf-8") as output:
+with open("结果.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
 
 # 写入更新日期时间
@@ -55,10 +55,10 @@ with open("iptv_list.txt", "w", encoding="utf-8") as output:
     #output.write(f"{now.strftime("%Y-%m-%d")},url\n")
     #output.write(f"{now.strftime("%H:%M:%S")},url\n")
 
-os.remove("iptv.txt")
+#os.remove("iptv.txt")
 os.remove("GAT.txt")
-os.remove("hn.txt")
-#os.remove("HK.txt")
-os.remove("DIYP-v4.txt")
+#os.remove("hn.txt")
+##os.remove("HK.txt")
+#os.remove("DIYP-v4.txt")
 os.remove("TW.txt")
-print("任务运行完毕，分类频道列表可查看文件夹内iptv_list.txt文件！")
+print("任务运行完毕，分类频道列表可查看文件夹内结果.txt文件！")
