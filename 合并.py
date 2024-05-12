d52bb5e4b4b2a6cf622e48119e1178170b5acb5b
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-12 17:26:54 +0800

    Create 合并.py

---
+++ 合并.py
@@ -0,0 +1,32 @@
+import time
+import concurrent.futures
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import requests
+import re
+import os
+import threading
+from queue import Queue
+from datetime import datetime
+
+# 合并自定义频道文件内容
+file_contents = []
+file_paths = ["广东电信.txt", "湖南电信.txt"]  # 替换为实际的文件路径列表
+for file_path in file_paths:
+    with open(file_path, 'r', encoding="utf-8") as file:
+        content = file.read()
+        file_contents.append(content)
+
+# 写入合并后的文件
+with open("iptv_list.txt", "w", encoding="utf-8") as output:
+    output.write('\n'.join(file_contents))
+
+# 写入更新日期时间
+    #now = datetime.now()
+    #output.write(f"更新时间,#genre#\n")
+    #output.write(f"{now.strftime("%Y-%m-%d")},url\n")
+    #output.write(f"{now.strftime("%H:%M:%S")},url\n")
+
+os.remove("广东电信.txt")
+os.remove("湖南电信.txt")
+print("任务运行完毕，分类频道列表可查看文件夹内iptv_list.txt文件！")
