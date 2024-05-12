f2724b535ccb57c0f5c88a6bb8ed75a6641820a4
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-12 18:55:06 +0800

    Update 提取.py

--- 提取.py
+++ 提取.py
@@ -13,7 +13,7 @@ from datetime import datetime
 url = "https://raw.githubusercontent.com/frxz751113/IPTVzb1/main/合并.txt"          #源采集地址
 r = requests.get(url)
 open('合并.txt','wb').write(r.content)         #打开源文件名
-                    if name and urld:
+if name and urld:
                             # 替换特定文字
                             name = name.replace("中央", "CCTV")
                             name = name.replace("高清", "")
@@ -116,10 +116,7 @@ open('合并.txt','wb').write(r.content)         #打开源文件名
                             name = name.replace("电视剧", "影视")
                             name = name.replace("卡", "")
                             results.append(f"{name},{urld}")
-            except:
-                continue
-        except:
-            continue
+
 
 channels = []
 
