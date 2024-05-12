707d51c500b9f0933dcacdd4cf4d830d3057c08c
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-12 18:11:30 +0800

    Update 提取.py

--- 提取.py
+++ 提取.py
@@ -10,22 +10,39 @@ from queue import Queue
 from datetime import datetime
 
 
-url = "https://raw.githubusercontent.com/frxz751113/IPTVzb1/main/广东电信.txt"          #源采集地址
+url = "https://raw.githubusercontent.com/frxz751113/IPTVzb1/main/合并.txt"          #源采集地址
 r = requests.get(url)
-open('广东电信.txt','wb').write(r.content)         #打开源文件名
-
+open('合并.txt','wb').write(r.content)         #打开源文件名
 keywords = ['重温经典', 'CCTV','热剧 8M1080', '超级电影 8M1080', '超级电视剧 8M1080', '喜剧 8M1080', '惊悚悬疑 8M1080', '明星大片 8M1080', '潮妈辣婆 8M1080', '精品大剧 8M1080', '动作电影 8M1080', '古装剧场 8M1080', '中国功夫 8M1080', '神乐剧场']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 #pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
-with open('广东电信.txt', 'r', encoding='utf-8') as file, open('TW.txt', 'w', encoding='utf-8') as TW:
-    TW.write('\n数字频道,#genre#\n')
+with open('合并电信.txt', 'r', encoding='utf-8') as file, open('TW.txt', 'w', encoding='utf-8') as TW:
+    TW.write('\n央视频道,#genre#\n')
     for line in file:
         if re.search(pattern, line):  # 如果行中有任意关键字
           TW.write(line)  # 将该行写入输出文件
 
+keywords = ['重温经典', '卫视','热剧 8M1080', '超级电影 8M1080', '超级电视剧 8M1080', '喜剧 8M1080', '惊悚悬疑 8M1080', '明星大片 8M1080', '潮妈辣婆 8M1080', '精品大剧 8M1080', '动作电影 8M1080', '古装剧场 8M1080', '中国功夫 8M1080', '神乐剧场']  # 需要提取的关键字列表
+pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+#pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
+with open('合并.txt', 'r', encoding='utf-8') as file, open('ws.txt', 'w', encoding='utf-8') as TW:
+    ws.write('\n卫视频道,#genre#\n')
+    for line in file:
+        if re.search(pattern, line):  # 如果行中有任意关键字
+          ws.write(line)  # 将该行写入输出文件
+
+keywords = ['重温经典', '影', '剧', '娱乐', '热剧 8M1080', '超级电影 8M1080', '超级电视剧 8M1080', '喜剧 8M1080', '惊悚悬疑 8M1080', '明星大片 8M1080', '潮妈辣婆 8M1080', '精品大剧 8M1080', '动作电影 8M1080', '古装剧场 8M1080', '中国功夫 8M1080', '神乐剧场']  # 需要提取的关键字列表
+pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+#pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
+with open('合并.txt', 'r', encoding='utf-8') as file, open('df.txt', 'w', encoding='utf-8') as TW:
+    df.write('\n地方频道,#genre#\n')
+    for line in file:
+        if re.search(pattern, line):  # 如果行中有任意关键字
+          df.write(line)  # 将该行写入输出文件
+
 # 读取要合并的香港频道和台湾频道文件
 file_contents = []
-file_paths = ["TW.txt"]  # 替换为实际的文件路径列表
+file_paths = ["TW.txt", "ws.txt", "df.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
@@ -55,10 +72,10 @@ with open("结果.txt", "w", encoding="utf-8") as output:
     #output.write(f"{now.strftime("%Y-%m-%d")},url\n")
     #output.write(f"{now.strftime("%H:%M:%S")},url\n")
 
-#os.remove("iptv.txt")
+os.remove("合并.txt")
 os.remove("GAT.txt")
-#os.remove("hn.txt")
-##os.remove("HK.txt")
+#os.remove("ws.txt")
+os.remove("df.txt")
 #os.remove("DIYP-v4.txt")
 os.remove("TW.txt")
 print("任务运行完毕，分类频道列表可查看文件夹内结果.txt文件！")
