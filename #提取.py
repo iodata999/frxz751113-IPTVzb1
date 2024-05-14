07877a622a6bfb8762915e1f6447229acd21a817
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-14 19:34:43 +0800

    Update #提取.py

--- #提取.py
+++ #提取.py
@@ -119,8 +119,8 @@ for line in fileinput.input("合并.txt", inplace=True):  #打开文件，并对
     print(line, end="")  #设置end=""，避免输出多余的换行符
     
     with open("合并.txt", "r") as file:
-    sorted_content = sorted(file, key=lambda line: line.strip())
-    for line in sorted_content:
+      sorted_content = sorted(file, key=lambda line: line.strip())
+      for line in sorted_content:
         print(line)
 
 
