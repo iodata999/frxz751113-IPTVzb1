f6d656bb796773e12554042bdf3339ada04426f2
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-21 21:38:59 +0800

    Update #组播综合.py

--- #组播综合.py
+++ #组播综合.py
@@ -16,7 +16,7 @@ import fileinput
 
 # 合并自定义频道文件#################################################################################################
 file_contents = []
-file_paths = ["四川电信.txt", "广东电信.txt", "安徽电信.txt"]  # 替换为实际的文件路径列表
+file_paths = ["四川电信.txt", "广东电信.txt", "#高质源.txt", "安徽电信.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
