6297aba71e82806ea0a9818caa28b01da11ed9cd
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-24 11:57:49 +0800

    Update #直播源检测.py

--- #直播源检测.py
+++ #直播源检测.py
@@ -12,11 +12,15 @@ import replace
 import fileinput
 import eventlet
 
+with open("iptvsearch/#自用.txt", 'r', encoding='utf-8') as file:
+ eventlet.monkey_patch()
 
+# 线程安全的队列，用于存储下载任务
+task_queue = Queue()
+# 线程安全的列表，用于存储结果
+results = []
 channels = []
 error_channels = []
-# 从iptv.txt文件内提取其他频道进行检测并分组
-with open("iptvsearch/#自用.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         line = line.strip()
