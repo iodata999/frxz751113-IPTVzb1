f2daa0fd315704d616c219576407882a13c4bbd8
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-24 14:08:01 +0800

    Update ZBYyz.py

--- ZBYyz.py
+++ ZBYyz.py
@@ -1,5 +1,5 @@
 
-for line in fileinput.input("DIYP-v4.txt", inplace=True):   #打开临时文件原地替换关键字
+for line in fileinput.input("iptvsearch/1.txt", inplace=True):   #打开临时文件原地替换关键字
     line = line.replace("输入原字符", "替换后的字符")                         #编辑替换字
     print(line, end="")                                     #加入此行去掉多余的转行符
 
