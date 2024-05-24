8383948f3c3bda4733e055a1d47eefb71d5937a6
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-24 19:08:43 +0800

    Update ZBYyz.py

--- ZBYyz.py
+++ ZBYyz.py
@@ -84,7 +84,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 128
+num_threads = 8
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
