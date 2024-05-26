3e03a68a3d2c26678aecf865a17b6b120f6d9b78
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-27 01:21:18 +0800

    Update txt2m3u.py

--- 组播py/txt2m3u.py
+++ 组播py/txt2m3u.py
@@ -11,7 +11,7 @@ from datetime import datetime
 
 input_file=结果.txt
 output_file=结果.m3u
-echo "#EXTM3U" > "$output_file"
+echo "#EXTM3U" > "$结果.m3u"
 
 # Pętla odczytująca każdą linię z pliku wejściowego
 while IFS= read -r line; do
@@ -20,9 +20,9 @@ while IFS= read -r line; do
     stream_url=$(echo "$line" | cut -d$'\t' -f2-)
 
     # Dodanie wpisu do pliku M3U
-    echo "#EXTINF:-1,$stream_name" >> "$output_file"
-    echo "$stream_url" >> "$output_file"
-done < "$input_file"
+    echo "#EXTINF:-1,$stream_name" >> "$结果.m3u"
+    echo "$stream_url" >> "$结果.m3u"
+done < "$结果.m3u"
 
-echo "Plik M3U został wygenerowany: $output_file"
+echo "Plik M3U został wygenerowany: $结果.m3u"
 print(f"成功寫出M3U file")
