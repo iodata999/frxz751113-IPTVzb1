67b45d7724fa402f7d5f6b4b6f823499744d1cec
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-27 00:26:51 +0800

    Update #组播综合.py

--- 组播py/#组播综合.py
+++ 组播py/#组播综合.py
@@ -570,7 +570,6 @@ with open("结果.txt", "w", encoding="utf-8") as output:
 with open("结果.m3u", 'w', encoding='utf-8') as file:
     channel_counters = {}
     file.write('#EXTM3U\n')
-    channel_name, channel_url = result
     if 'CCTV' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
@@ -578,7 +577,6 @@ with open("结果.m3u", 'w', encoding='utf-8') as file:
                  file.write(f"{channel_url}\n")
     channel_counters = {}
     #file.write('卫视频道,#genre#\n')
-    channel_name, channel_url = result
     if '卫视' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
@@ -586,7 +584,6 @@ with open("结果.m3u", 'w', encoding='utf-8') as file:
                  file.write(f"{channel_url}\n")
     channel_counters = {}
     #file.write('其他频道,#genre#\n')
-    channel_name, channel_url = result
     if 'CCTV' not in channel_name and '卫视' not in channel_name and '测试' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
