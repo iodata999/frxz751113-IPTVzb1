c05b27be8f9c6f47781feb7069d44162ffceaff3
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-26 22:19:23 +0800

    Update #组播综合.py

--- 组播py/#组播综合.py
+++ 组播py/#组播综合.py
@@ -569,10 +569,9 @@ with open("结果.txt", "w", encoding="utf-8") as output:
 with open("结果.m3u", 'w', encoding='utf-8') as file:
         channel_counters = {}
         file.write('#EXTM3U\n')
-if channel_name in channel_counters:
-        if channel_counters[channel_name] >= result_counter:
-                    file.write(f"#EXTINF:-1,{channel_name}\n")
-                    file.write(f"{channel_url}\n")
+if channel_name in result_counter:
+         file.write(f"#EXTINF:-1,{channel_name}\n")
+         file.write(f"{channel_url}\n")
 
 
 
