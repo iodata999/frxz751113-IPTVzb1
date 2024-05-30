8daf71110ec64fa9154e1a931cc31a796e37482f
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-30 15:06:00 +0800

    Update 酒店源验证.py

--- 趣味小程序/酒店源验证.py
+++ 趣味小程序/酒店源验证.py
@@ -50,7 +50,7 @@ def worker():
             ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
 
             # 多获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(15, False):  #################////////////////////////////////
+            with eventlet.Timeout(10, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -85,7 +85,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 8
+num_threads = 4
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
