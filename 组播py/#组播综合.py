0860eb90fedd5576e41ec1a2666eb552fb56a539
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-01 22:16:31 +0800

    Update #组播综合.py

--- 组播py/#组播综合.py
+++ 组播py/#组播综合.py
@@ -256,6 +256,12 @@ with open('排序.txt', 'r', encoding='utf-8') as file, open('c.txt', 'w', encod
          c.write(line)  # 将该行写入输出文件                                                          #####定义临时文件
 for line in fileinput.input("c.txt", inplace=True):  #打开文件，并对其进行关键词原地替换                     ###########
     line = line.replace("AA", "")                                                                         ###########                                                      ###########
+    line = line.replace("综合文艺", "")                                                                         ###########                                                      ###########
+    line = line.replace("科教", "")                                                                         ###########                                                      ###########
+    line = line.replace("社会与法", "")                                                                         ###########                                                      ###########
+    line = line.replace("新闻", "")                                                                         ###########                                                      ###########
+    line = line.replace("少儿", "")                                                                         ###########                                                      ###########
+    line = line.replace("AA", "")                                                                         ###########                                                      ###########
     line = line.replace("地理世界", "世界地理")                                                                         ###########                                                      ###########
     print(line, end="")  #设置end=""，避免输出多余的换行符          
 
