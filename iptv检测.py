39530175b56798c48d5ea4f2b6cc6a9009367ece
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-06 08:41:51 +0800

    Update iptv检测.py

--- iptv检测.py
+++ iptv检测.py
@@ -13,18 +13,18 @@ import fileinput
 import eventlet
 
 
-with open("V4汇总.txt", 'r', encoding='utf-8') as file:
-    eventlet.monkey_patch()
+eventlet.monkey_patch()
 # 线程安全的队列，用于存储下载任务
-    task_queue = Queue()
+task_queue = Queue()
 
 # 线程安全的列表，用于存储结果
-    results = []
+results = []
 
-    channels = []
-    error_channels = []
+channels = []
+error_channels = []
 # 从iptv.txt文件内提取其他频道进行检测并分组
-
+with open("V4汇总.txt", 'r', encoding='utf-8') as file:
+   
     lines = file.readlines()
     for line in lines:
         line = line.strip()
@@ -48,7 +48,7 @@ def worker():
             
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(6, False):  #################////////////////////////////////
+            with eventlet.Timeout(12, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -83,7 +83,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 4
+num_threads = 8
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
@@ -109,7 +109,7 @@ def channel_key(channel_name):
 # 对频道进行排序
 results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 results.sort(key=lambda x: channel_key(x[0]))
-result_counter = 8  # 每个频道需要的个数
+result_counter = 3  # 每个频道需要的个数
 
 with open("hn.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
