ad2c6111da6aac8ce25da11ae94edf68dbdcf762
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-08 00:10:27 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -401,7 +401,7 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
     file.write('央视频道/自动更新,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if 'CCTV' in channel_name  或者 '动作' in channel_name  或者 '家庭' in channel_name  或者 '热播' in channel_name  或者 '经典' in channel_name  或者 '谍战' in channel_name  或者 '电影,' in channel_name:
+        if 'CCTV' in channel_name or '动作' in channel_name or '家庭' in channel_name or '热播' in channel_name or '经典' in channel_name or '谍战' in channel_name or '电影,' in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
@@ -426,6 +426,8 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
             else:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
+
+
 
 
 
@@ -455,9 +457,9 @@ for file_path in file_paths:
         file_contents.append(content)
 
 # 写入合并后的文件
-with open("酒店源.txt", "w", encoding="utf-8") as output:
+with open("iptv_list.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
-for line in fileinput.input("酒店源.txt", inplace=True):  #打开文件，并对其进行关键词原地替换 
+for line in fileinput.input("iptv_list.txt", inplace=True):  #打开文件，并对其进行关键词原地替换 
     line = line.replace("AA", "")
     line = line.replace("电影,", "高清电影,")
     print(line, end="")  #设置end=""，避免输出多余的换行符          
