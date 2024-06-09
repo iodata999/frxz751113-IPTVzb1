7cc042804d01028b39cce74f921880c2cdebdfbf
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-09 23:24:02 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -415,6 +415,21 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
+    channel_counters = {}
+    file.write('其他频道/自动更新,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if 'CCTV' 不 in channel_name 和 '卫视' 不 in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+
 
 
 
