744779282a251cc0fda3f446871ab3763ceeec36
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-09 09:35:12 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -461,6 +461,7 @@ with open("酒店源.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
 for line in fileinput.input("酒店源.txt", inplace=True):  #打开文件，并对其进行关键词原地替换 
     line = line.replace("AA", "")
+    line = line.replace("央视频道/固定源,#genre#", "")
     print(line, end="")  #设置end=""，避免输出多余的换行符          
 
 
