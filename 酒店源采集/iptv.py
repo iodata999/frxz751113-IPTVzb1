13c536a0f3a4d5773ec6a0086bc4e2bb8e77d377
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-09 12:00:16 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -333,7 +333,7 @@ def worker():
             
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(20, False):  #################////////////////////////////////
+            with eventlet.Timeout(10, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -368,7 +368,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 8
+num_threads = 4
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
