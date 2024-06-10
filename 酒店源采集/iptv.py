9ac04351ccbae770de9362b9d4edefebf3c867fa
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-10 11:03:17 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -371,7 +371,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 60
+num_threads = 16
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
