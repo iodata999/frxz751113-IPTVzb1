abceb7a84bcbee77bc57d8ec961ef8386c0a623f
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-10 09:01:02 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -430,7 +430,7 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
     file.write('其他频道/自动更新,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if '广' in channel_name or '吉' in channel_name or '黑龙江' in channel_name or '南宁' in channel_name:
+        if '广' in channel_name or '黑龙江' in channel_name or '南宁' in channel_name:
         #if 'CCTV' not in channel_name and '卫视' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
