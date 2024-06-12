6002f1c8e8a1ab35e76e0405bc2f15f6e5d07f8d
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-12 19:09:21 +0800

    Delete 趣味小程序 directory

--- 趣味小程序/UTF-8排序.py
+++
@@ -1,9 +0,0 @@
-
-with open('1.txt', 'r', encoding='UTF-8') as f:
-    lines = f.readlines()
-
-lines.sort()
-
-with open('新.txt', 'w', encoding='UTF-8') as f:
-    for line in lines:
-        f.write(line)
