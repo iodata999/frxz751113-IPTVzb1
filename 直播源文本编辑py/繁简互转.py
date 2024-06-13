6a8b06a7db4d61b638e549ed07cdaeaa50ebd148
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-13 12:32:52 +0800

    Update 繁简互转.py

--- 直播源文本编辑py/繁简互转.py
+++ 直播源文本编辑py/繁简互转.py
@@ -5,12 +5,12 @@ from opencc import OpenCC
 #converter = OpenCC('t2s.json')#繁转简
 converter = OpenCC('s2t.json')#简转繁
 # 打开itv.txt文件
-with open('itv.txt', 'r', encoding='utf-8') as file:
+with open('结果.txt', 'r', encoding='utf-8') as file:
     traditional_text = file.read()
 
 # 进行繁体字转简体字的转换
 simplified_text = converter.convert(traditional_text)
 
 # 将转换后的简体字写入itv.txt文件
-with open('itv.txt', 'w', encoding='utf-8') as file:
+with open('i结果.txt', 'w', encoding='utf-8') as file:
     file.write(simplified_text)
