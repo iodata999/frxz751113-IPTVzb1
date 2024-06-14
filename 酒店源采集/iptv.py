c4326e6ffd8692e151aa823629304b6e25390769
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-14 16:37:40 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -501,6 +501,9 @@ with open("酒店源.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
 for line in fileinput.input("酒店源.txt", inplace=True):  #打开文件，并对其进行关键词原地替换 
     line = line.replace("AA", "")
+    line = line.replace("CCTV动", "动")
+    line = line.replace("CCTV家", "家")
+    line = line.replace("CCTV影", "影")
     line = line.replace("\n电影,", "\n影迷电影,")
     print(line, end="")  #设置end=""，避免输出多余的换行符          
 #########原始顺序去重，以避免同一个频道出现在不同的类中
