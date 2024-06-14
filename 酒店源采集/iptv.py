7facbc24f0a4280885a48636263d3c700c67bb04
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-15 00:22:23 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -359,7 +359,7 @@ def worker():
             
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(50, False):  #################////////////////////////////////
+            with eventlet.Timeout(20, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
