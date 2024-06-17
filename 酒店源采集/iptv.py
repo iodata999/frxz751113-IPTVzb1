f510491053337c6a1d79c71a33ac03f7db302b64
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-17 08:34:42 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -78,7 +78,7 @@ def modify_urls(url):
 
 def is_url_accessible(url):
     try:
-        response = requests.get(url, timeout=0.3)          ###//////////////////
+        response = requests.get(url, timeout=1)          ###//////////////////
         if response.status_code == 200:
             return url
     except requests.exceptions.RequestException:
@@ -156,7 +156,7 @@ for url in urls:
             url_x = f"{base_url}{ip_address}"
 
             json_url = f"{url}"
-            response = requests.get(json_url, timeout=0.3)                        ####///////////////
+            response = requests.get(json_url, timeout=1)                        ####///////////////
             json_data = response.json()
 
             try:
@@ -361,7 +361,7 @@ def worker():
             
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(20, False):  #################////////////////////////////////
+            with eventlet.Timeout(8, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -396,7 +396,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 32
+num_threads = 8
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
