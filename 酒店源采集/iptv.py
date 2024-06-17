64be49b96f7d57469ea4fdf6a9c8b87c4a3d2410
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-17 16:19:17 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -170,7 +170,7 @@ for url in urls:
 
                         #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
                         if 'http' in urlx:
-                          if 'udp' not in urlx:
+                          if 'udp' not in urlx and '171.108.239.77' not in urlx:
                             urld = f"{urlx}"
                         else:
                             urld = f"{url_x}{urlx}"
