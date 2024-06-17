b0cfa6e868f229fe09920f8688db63be204b4d81
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-18 03:16:53 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -87,7 +87,7 @@ def modify_urls(url):
 
 def is_url_accessible(url):
     try:
-        response = requests.get(url, timeout=0.5)          ###//////////////////
+        response = requests.get(url, timeout=0.1)          ###//////////////////
         if response.status_code == 200:
             return url
     except requests.exceptions.RequestException:
@@ -165,7 +165,7 @@ for url in urls:
             url_x = f"{base_url}{ip_address}"
 
             json_url = f"{url}"
-            response = requests.get(json_url, timeout=0.5)                        ####///////////////
+            response = requests.get(json_url, timeout=0.1)                        ####///////////////
             json_data = response.json()
 
             try:
