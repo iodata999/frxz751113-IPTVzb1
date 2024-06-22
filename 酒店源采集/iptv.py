eaa58529ee6da0e88ed26826e50a915498efb7c1
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-22 11:05:41 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -519,7 +519,21 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
             else:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
-
+    channel_counters = {}
+    file.write('湖北动漫,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if '河北' in channel_name:
+          if 'CCTV' not in channel_name and '卫视' not in channel_name and '购物' not in channel_name:  
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
 
     channel_counters = {}
     file.write('湖北动漫,#genre#\n')
