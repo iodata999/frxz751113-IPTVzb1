eb0eff7eb1d7c8ab263e6aa562d8f701ebc41c75
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-25 03:25:28 +0800

    Update ZHGX.py

--- 酒店源采集/ZHGX.py
+++ 酒店源采集/ZHGX.py
@@ -141,7 +141,7 @@ for url in urls:
                             name = name.replace("标清", "")
                             name = name.replace("超高", "")
                             name = name.replace("频道", "")
-                            name = name.replace("靓装", "女性时尚")
+                            name = name.replace("靓妆", "女性时尚")
                             name = name.replace("汉2", "汉")
                             name = name.replace("汉3", "汉")
                             name = name.replace("汉4", "汉")
