4227f8cfe070a072a0817719f21bc60622e425a1
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-25 01:50:45 +0800

    Update ZHGX.py

--- 酒店源采集/ZHGX.py
+++ 酒店源采集/ZHGX.py
@@ -397,10 +397,10 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
                 channel_counters[channel_name] = 1
 
     channel_counters = {}
-    file.write('省市动漫,#genre#\n')
+    file.write('省市频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if '湖北' in channel_name or '四川' in channel_name  or '深圳' in channel_name or '河北' in channel_name or '广东' in channel_name or '广西' in channel_name or '卡通' in channel_name or '少儿' in channel_name or '哈哈' in channel_name or '动漫' in channel_name:
+        if '湖北' in channel_name or '四川' in channel_name  or '深圳' in channel_name or '河北' in channel_name or '广东' in channel_name or '广西' in channel_name:
           if 'CCTV' not in channel_name and '卫视' not in channel_name and '购物' not in channel_name:  
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
@@ -412,7 +412,22 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
                 file.write(f"{channel_name},{channel_url}\n")
                 channel_counters[channel_name] = 1
 
-    
+    channel_counters = {}
+    file.write('少儿动漫,#genre#\n')
+    for result in results:
+        channel_name, channel_url, speed = result
+        if '卡通' in channel_name or '少儿' in channel_name or '哈哈' in channel_name or '动漫' in channel_name or '动画' in channel_name:
+          if 'CCTV' not in channel_name and '卫视' not in channel_name and '湖' not in channel_name and '广' not in channel_name and '河' not in channel_name and '黑' not in channel_name and '保' not in channel_name and '宁' not in channel_name and '家庭' not in channel_name and '影迷' not in channel_name and '动作' not in channel_name and '武汉' not in channel_name and 'CETV' not in channel_name and '交通' not in channel_name and '冬' not in channel_name:
+            if channel_name in channel_counters:
+                if channel_counters[channel_name] >= result_counter:
+                    continue
+                else:
+                    file.write(f"{channel_name},{channel_url}\n")
+                    channel_counters[channel_name] += 1
+            else:
+                file.write(f"{channel_name},{channel_url}\n")
+                channel_counters[channel_name] = 1
+        
 
 
 
