b73b3314fa5feeac3fd842e8d03472a0ec2d4f04
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-29 18:50:45 +0800

    Update ZHGX.py

--- 酒店源采集/ZHGX.py
+++ 酒店源采集/ZHGX.py
@@ -22,7 +22,7 @@ import fileinput
 urls = [
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJndWFuZ2Rvbmci",#广东
     "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJoZWJlaSI%3D",#河北
-    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJzaWNodWFuIg%3D%3D",#四川  
+    #"https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJzaWNodWFuIg%3D%3D",#四川  
 ]
 def modify_urls(url):
     modified_urls = []
