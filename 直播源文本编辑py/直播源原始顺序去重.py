5f13f5135bfb7acc6dd4cf32907ec47cdb3dbdc2
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-09 20:31:31 +0800

    Update and rename 直播源去重.py to 直播源原始顺序去重.py

--- 直播源文本编辑py/直播源原始顺序去重.py
+++ 直播源文本编辑py/直播源原始顺序去重.py
@@ -3,10 +3,15 @@ a = input('FileName(DragHere):')
 with open(a, 'r', encoding="utf-8") as f:
     lines = f.readlines()
     before = len(lines)
-    lines = list(set(lines))
-    after = len(lines)
+    seen = set()
+    unique_lines = []
+    for line in lines:
+        if line not in seen:
+            unique_lines.append(line)
+            seen.add(line)
+    after = len(unique_lines)
 with open(a, 'w', encoding="utf-8") as f:
-    f.writelines(lines)
+    f.writelines(unique_lines)
 
 print('处理完成：')
 print(f'处理前文件行数：{before}')
