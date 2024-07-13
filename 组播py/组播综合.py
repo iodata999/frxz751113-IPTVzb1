364c58391aeaaa0f6b9642c189bdfab325a1991e
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-13 13:44:45 +0800

    Update 组播综合.py

--- 组播py/组播综合.py
+++ 组播py/组播综合.py
@@ -468,6 +468,48 @@ for line in fileinput.input("临时组播.txt", inplace=True):  #打开文件，
     print(line, end="")  #设置end=""，避免输出多余的换行符
 
 
+########################################
+
+
+from pypinyin import lazy_pinyin
+
+# 打开一个utf-8编码的文本文件
+with open("临时组播.txt", "r", encoding="utf-8") as file:
+    # 读取所有行并存储到列表中
+    lines = file.readlines()
+
+# 定义一个函数，用于提取每行的第一个数字
+def extract_first_number(line):
+    match = re.search(r'\d+', line)
+    return int(match.group()) if match else float('inf')
+
+# 对列表中的行进行排序，按照第一个数字的大小排列，其余行按中文排序
+sorted_lines = sorted(lines, key=lambda x: (not 'CCTV' in x, extract_first_number(x) if 'CCTV' in x else lazy_pinyin(x.strip())))
+
+# 将排序后的行写入新的utf-8编码的文本文件
+with open("临时组播.txt", "w", encoding="utf-8") as file:
+    for line in sorted_lines:
+        file.write(line)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #从整理好的文本中按类别进行特定关键词提取#############################################################################################
 keywords = ['环绕']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
