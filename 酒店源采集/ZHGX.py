ea6ad3b6ef2ab210ece315f2c315bcb9f340beca
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-13 11:14:53 +0800

    Update ZHGX.py

--- 酒店源采集/ZHGX.py
+++ 酒店源采集/ZHGX.py
@@ -303,7 +303,7 @@ def worker():
             
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(64, False):  #################////////////////////////////////
+            with eventlet.Timeout(12, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -338,7 +338,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 16
+num_threads = 32
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
