a0be7f1d932f8c99133042e10688ec52b4ad4d6d
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-14 20:05:01 +0800

    Update XXX去重排序分类终版.py

--- 组播py/XXX去重排序分类终版.py
+++ 组播py/XXX去重排序分类终版.py
@@ -217,7 +217,7 @@ for line in lines:
   seen_lines.add(line)
 
 # 将唯一的行写入新的文档 
-with open('组播源1.txt', 'w', encoding="utf-8") as file:
+with open('组播源.txt', 'w', encoding="utf-8") as file:
  file.writelines(unique_lines)
 ##############################
 
