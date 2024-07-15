8bc19cccb762397769feb4a38bcdd972b5ac6a72
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-15 14:58:57 +0800

    Update 组播综合1.py

--- 组播py/组播综合1.py
+++ 组播py/组播综合1.py
@@ -15,34 +15,6 @@ from opencc import OpenCC
 import os
 import glob
 
-def merge_files(folder_path, output_file):
-    # 获取文件夹中的所有文件
-    files = glob.glob(os.path.join(folder_path, '*'))
-
-    # 创建一个新的文件用于合并
-    with open(output_file, 'w') as outfile:
-        # 遍历文件夹中的每个文件
-        for file in files:
-            # 打开文件并读取内容
-            with open(file, 'r') as infile:
-                # 将文件内容写入到新的文件中
-                outfile.write(infile.read())
-                # 添加换行符以分隔不同文件的内容
-                outfile.write('')
-
-# 使用示例
-folder_path = 'outfiles'  # 替换为你的文件夹路径
-output_file = '组播源.txt'  # 替换为你想要保存合并后的文件名
-merge_files(folder_path, output_file)
-
-for line in fileinput.input("组播源.txt", inplace=True):  #打开文件，并对其进行关键词原地替换 
-    line = line.replace("CHC电影", "CHC影迷电影") 
-    line = line.replace("高清电影", "影迷电影") 
-    print(line, end="")  #设置end=""，避免输出多余的换行符   
-
-
-
-
 
 
 #从整理好的文本中按类别进行特定关键词提取#############################################################################################
