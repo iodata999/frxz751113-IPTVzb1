97f73a666b4b412012ad79d2a1060d5dac60cf4a
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-19 00:04:45 +0800

    Update iptv1.py

--- 酒店源采集/iptv1.py
+++ 酒店源采集/iptv1.py
@@ -22,7 +22,7 @@ pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个
 with open('ott移动v4.txt', 'r', encoding='utf-8') as file, open('TW.txt', 'w', encoding='utf-8') as TW:
     #TW.write('\n央视频道,#genre#\n')
     for line in file:
-      for "genre" not in line:
+      if "genre" not in line:
         if re.search(pattern, line):  # 如果行中有任意关键字
           TW.write(line)  # 将该行写入输出文件
 
