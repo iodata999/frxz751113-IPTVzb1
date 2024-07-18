8f5faa6c5721fc9f1282bb0fd76a564d2963ddeb
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-07-18 16:58:36 +0800

    Update 极速验源.py

--- 酒店源采集/极速验源.py
+++ 酒店源采集/极速验源.py
@@ -17,7 +17,7 @@ open('1.txt','wb').write(r.content)         #打开源文件并临时写入
 
 def test_connectivity(url):
     try:
-        response = requests.get(url, timeout=15)
+        response = requests.get(url, timeout=3)
         return response.status_code == 200
     except requests.RequestException:
         return False
