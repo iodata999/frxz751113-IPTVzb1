fd271f0ed710f2e0522c0136f831b77ebf7973db
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-02 03:03:26 +0800

    Update 组播综合.py

--- 组播py/组播综合.py
+++ 组播py/组播综合.py
@@ -564,7 +564,7 @@ txt_to_m3u('综合源.txt', '综合源.m3u')
 
 
 #任务结束，删除不必要的过程文件###########################################################################################################################
-files_to_remove = ['湖南电信.txt', '广东电信.txt', '四川电信.txt', '天津联通.txt', '组播源.txt', "GAT.txt", "TW.txt", "a.txt", "a0.txt", "b.txt", "b1.txt", "港澳.txt", "df0.txt", "df.txt", "df1.txt", "sr1.txt", "sr2.txt", \
+files_to_remove = ['湖南电信.txt', '广东电信.txt', '四川电信.txt', '河南电信.txt', '河北电信.txt', '天津联通.txt', '组播源.txt', "GAT.txt", "TW.txt", "a.txt", "a0.txt", "b.txt", "b1.txt", "港澳.txt", "df0.txt", "df.txt", "df1.txt", "sr1.txt", "sr2.txt", \
                    "c2.txt", "c1.txt", "DD.txt", "f.txt", "f1.txt", "ott移动v4.txt"]
 for file in files_to_remove:
     if os.path.exists(file):
