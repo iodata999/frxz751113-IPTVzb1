f7728600eed66c47fe12cfcbaac2289bd0dc30be
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-08-04 20:31:15 +0800

    Update 合并.py

--- 组播py/合并.py
+++ 组播py/合并.py
@@ -10,3 +10,35 @@ file2_path = 'yeye.txt'
 output_path = 'merged.txt'  
 
 merge_txt_files(file1_path, file2_path, output_path)
+
+######################TXT转M3U#####################################################################################################################################################
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"\n')
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            if "," in line:  # 防止文件里面缺失“,”号报错
+                # if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 tvg-id="{channel_name}" tvg-name="{channel_name}" tvg-logo="https://live.fanmingming.com/tv/{channel_name}.png" group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
+
+
+# 将txt文件转换为m3u文件
+txt_to_m3u('iptv_list.txt', 'iptv_list.m3u')
+
