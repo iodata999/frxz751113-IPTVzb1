1326bdefd3adbbb42ff8a1adca1d1816ded9c0d6
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-05 19:33:58 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -1205,7 +1205,7 @@ with open('酒店源.txt', 'r', encoding='utf-8') as f:  #打开文件，并对�
  pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字                                      ###########
  #pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制                                                     ###########
  with open('酒店源.txt', 'r', encoding='utf-8') as file, open('b.txt', 'w', encoding='utf-8') as b:           ###########
-    b.write('\港澳频道,#genre#\n')                                                                        ###########
+    b.write('港澳频道,#genre#\n')                                                                        ###########
     for line in file:                                                                                      ###########
         if re.search(pattern, line):  # 如果行中有任意关键字                                                ###########
           b.write(line)  # 将该行写入输出文件                                                               ###########
