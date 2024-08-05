979d2c004ea741b1a6fb56b9bf74a9836331fc57
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-08-05 16:03:58 +0800

    Update iptv.py

--- 酒店源采集/iptv.py
+++ 酒店源采集/iptv.py
@@ -1,4 +1,4 @@
-#本程序只适用于光迅源的检测，请勿移植他用
+#本程序只适用于酒店源的检测，请勿移植他用
 import time
 import concurrent.futures
 from selenium import webdriver
@@ -19,6 +19,237 @@ from opencc import OpenCC
 # 搜素关键词："iptv/live/zh_cn.js" && country="CN" && region="Hunan" && city="changsha"
 # 搜素关键词："ZHGXTV" && country="CN" && region="Hunan" && city="changsha"
 #"isShowLoginJs"智能KUTV管理
+
+urls = [
+    #"https://fofa.info/result?qbase64=anNfbmFtZT0iL1pIR1hUVi9QdWJsaWMvbGF5dWkvbGF5dWkuanMi",#搜索词js_name="/ZHGXTV/Public/layui/layui.js"
+    "https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJndWFuZ2Rvbmci",#广东
+    #"https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJIZW5hbiI%3D",#河南
+    #"https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJoZWJlaSI%3D",#河北
+    #"https://fofa.info/result?qbase64=IlpIR1hUViIgJiYgcmVnaW9uPSJzaWNodWFuIg%3D%3D",#四川  
+]
+def modify_urls(url):
+    modified_urls = []
+    ip_start_index = url.find("//") + 2
+    ip_end_index = url.find(":", ip_start_index)
+    base_url = url[:ip_start_index]  # http:// or https://
+    ip_address = url[ip_start_index:ip_end_index]
+    port = url[ip_end_index:]
+    ip_end = "/ZHGXTV/Public/json/live_interface.txt"
+    for i in range(1, 256):
+        modified_ip = f"{ip_address[:-1]}{i}"
+        modified_url = f"{base_url}{modified_ip}{port}{ip_end}"
+        modified_urls.append(modified_url)
+
+    return modified_urls
+
+
+def is_url_accessible(url):
+    try:
+        response = requests.get(url, timeout=2)          ###//////////////////
+        if response.status_code == 200:
+            return url
+    except requests.exceptions.RequestException:
+        pass
+    return None
+
+
+results = []
+
+for url in urls:
+    # 创建一个Chrome WebDriver实例
+    chrome_options = Options()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+
+    driver = webdriver.Chrome(options=chrome_options)
+    # 使用WebDriver访问网页
+    driver.get(url)  # 将网址替换为你要访问的网页地址
+    time.sleep(10)
+    # 获取网页内容
+    page_content = driver.page_source
+
+    # 关闭WebDriver
+    driver.quit()
+
+    # 查找所有符合指定格式的网址
+    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
+    urls_all = re.findall(pattern, page_content)
+    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
+    urls = set(urls_all)  # 去重得到唯一的URL列表
+    x_urls = []
+    for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+        url = url.strip()
+        ip_start_index = url.find("//") + 2
+        ip_end_index = url.find(":", ip_start_index)
+        ip_dot_start = url.find(".") + 1
+        ip_dot_second = url.find(".", ip_dot_start) + 1
+        ip_dot_three = url.find(".", ip_dot_second) + 1
+        base_url = url[:ip_start_index]  # http:// or https://
+        ip_address = url[ip_start_index:ip_dot_three]
+        port = url[ip_end_index:]
+        ip_end = "1"
+        modified_ip = f"{ip_address}{ip_end}"
+        x_url = f"{base_url}{modified_ip}{port}"
+        x_urls.append(x_url)
+    urls = set(x_urls)  # 去重得到唯一的URL列表
+
+    valid_urls = []
+    #   多线程获取可用url
+    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+        futures = []
+        for url in urls:
+            url = url.strip()
+            modified_urls = modify_urls(url)
+            for modified_url in modified_urls:
+                futures.append(executor.submit(is_url_accessible, modified_url))
+
+        for future in concurrent.futures.as_completed(futures):
+            result = future.result()
+            if result:
+                valid_urls.append(result)
+
+    for url in valid_urls:
+        print(url)
+    # 遍历网址列表，获取JSON文件并解析
+    for url in valid_urls:
+        try:
+            # 发送GET请求获取JSON文件，设置超时时间为0.5秒
+            json_url = f"{url}"
+            response = requests.get(json_url, timeout=3)################################
+            json_data = response.content.decode('utf-8')
+            try:
+                    # 按行分割数据
+             lines = json_data.split('\n')
+             for line in lines:
+                 ######################
+                 if 'udp' not in line and 'rtp' not in line:
+                 #########################
+                        line = line.strip()
+                        if line:
+                            name, channel_url = line.split(',')
+                            urls = channel_url.split('/', 3)
+                            url_data = json_url.split('/', 3)
+                            if len(urls) >= 4:
+                                urld = (f"{urls[0]}//{url_data[2]}/{urls[3]}")
+                            else:
+                                urld = (f"{urls[0]}//{url_data[2]}")
+                            print(f"{name},{urld}")
+
+                        if name and urld:
+                            name = name.replace("高清电影", "影迷电影")                            
+                            name = name.replace("中央", "CCTV")
+                            name = name.replace("高清", "")
+                            name = name.replace("HD", "")
+                            name = name.replace("标清", "")
+                            name = name.replace("超高", "")
+                            name = name.replace("频道", "")
+                            name = name.replace("靓妆", "女性时尚")
+                            name = name.replace("本港台", "TVB星河")
+                            name = name.replace("汉3", "汉")
+                            name = name.replace("汉4", "汉")
+                            name = name.replace("汉5", "汉")
+                            name = name.replace("汉6", "汉")
+                            name = name.replace("CHC动", "动")
+                            name = name.replace("CHC家", "家")
+                            name = name.replace("CHC影", "影")
+                            name = name.replace("-", "")
+                            name = name.replace(" ", "")
+                            name = name.replace("PLUS", "+")
+                            name = name.replace("＋", "+")
+                            name = name.replace("(", "")
+                            name = name.replace(")", "")
+                            name = name.replace("L", "")
+                            name = name.replace("CCTVNEWS", "CCTV13")
+                            name = name.replace("cctv", "CCTV")
+                            name = re.sub(r"CCTV(\d+)台", r"CCTV\1", name)
+                            name = name.replace("CCTV1综合", "CCTV1")
+                            name = name.replace("CCTV2财经", "CCTV2")
+                            name = name.replace("CCTV3综艺", "CCTV3")
+                            name = name.replace("CCTV4国际", "CCTV4")
+                            name = name.replace("CCTV4中文国际", "CCTV4")
+                            name = name.replace("CCTV4欧洲", "CCTV4")
+                            name = name.replace("CCTV5体育", "CCTV5")
+                            name = name.replace("CCTV5+体育", "CCTV5+")
+                            name = name.replace("CCTV6电影", "CCTV6")
+                            name = name.replace("CCTV7军事", "CCTV7")
+                            name = name.replace("CCTV7军农", "CCTV7")
+                            name = name.replace("CCTV7农业", "CCTV7")
+                            name = name.replace("CCTV7国防军事", "CCTV7")
+                            name = name.replace("CCTV8电视剧", "CCTV8")
+                            name = name.replace("CCTV8纪录", "CCTV9")
+                            name = name.replace("CCTV9记录", "CCTV9")
+                            name = name.replace("CCTV9纪录", "CCTV9")
+                            name = name.replace("CCTV10科教", "CCTV10")
+                            name = name.replace("CCTV11戏曲", "CCTV11")
+                            name = name.replace("CCTV12社会与法", "CCTV12")
+                            name = name.replace("CCTV13新闻", "CCTV13")
+                            name = name.replace("CCTV新闻", "CCTV13")
+                            name = name.replace("CCTV14少儿", "CCTV14")
+                            name = name.replace("央视14少儿", "CCTV14")
+                            name = name.replace("CCTV少儿超", "CCTV14")
+                            name = name.replace("CCTV15音乐", "CCTV15")
+                            name = name.replace("CCTV音乐", "CCTV15")
+                            name = name.replace("CCTV16奥林匹克", "CCTV16")
+                            name = name.replace("SCTV5四川影视）", "SCTV5")
+                            name = name.replace("CCTV17农业农村", "CCTV17")
+                            name = name.replace("CCTV17军农", "CCTV17")
+                            name = name.replace("CCTV17农业", "CCTV17")
+                            name = name.replace("CCTV5+体育赛视", "CCTV5+")
+                            name = name.replace("CCTV5+赛视", "CCTV5+")
+                            name = name.replace("CCTV5+体育赛事", "CCTV5+")
+                            name = name.replace("CCTV5+赛事", "CCTV5+")
+                            name = name.replace("CCTV5+体育", "CCTV5+")
+                            name = name.replace("CCTV5赛事", "CCTV5+")
+                            name = name.replace("凤凰中文台", "凤凰中文")
+                            name = name.replace("凤凰资讯台", "凤凰资讯")
+                            name = name.replace("CCTV4K测试）", "CCTV4")
+                            name = name.replace("CCTV164K", "CCTV16")
+                            name = name.replace("上海东方卫视", "上海卫视")
+                            name = name.replace("东方卫视", "上海卫视")
+                            name = name.replace("内蒙卫视", "内蒙古卫视")
+                            name = name.replace("福建东南卫视", "东南卫视")
+                            name = name.replace("广东南方卫视", "南方卫视")
+                            name = name.replace("湖南金鹰卡通", "金鹰卡通")
+                            name = name.replace("炫动卡通", "哈哈炫动")
+                            name = name.replace("卡酷卡通", "卡酷少儿")
+                            name = name.replace("卡酷动画", "卡酷少儿")
+                            name = name.replace("BRTVKAKU少儿", "卡酷少儿")
+                            name = name.replace("优曼卡通", "优漫卡通")
+                            name = name.replace("优曼卡通", "优漫卡通")
+                            name = name.replace("嘉佳卡通", "佳嘉卡通")
+                            name = name.replace("世界地理", "地理世界")
+                            name = name.replace("CCTV世界地理", "地理世界")
+                            name = name.replace("BTV北京卫视", "北京卫视")
+                            name = name.replace("BTV冬奥纪实", "冬奥纪实")
+                            name = name.replace("东奥纪实", "冬奥纪实")
+                            name = name.replace("卫视台", "卫视")
+                            name = name.replace("湖南电视台", "湖南卫视")
+                            name = name.replace("少儿科教", "少儿")
+                            name = name.replace("TV星河2）", "星河")
+                            name = name.replace("影视剧", "影视")
+                            name = name.replace("电视剧", "影视")
+                            name = name.replace("奥运匹克", "")
+                            results.append(f"{name},{urld}")
+            except:
+                continue
+        except:
+            continue
+
+channels = []
+
+for result in results:
+    line = result.strip()
+    if result:
+        channel_name, channel_url = result.split(',')
+        channels.append((channel_name, channel_url))
+
+with open("iptv.txt", 'w', encoding='utf-8') as file:
+    for result in results:
+        file.write(result + "\n")
+        print(result)
+print("频道列表文件iptv.txt获取完成！")
+
 urls = [
     #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgcG9ydD0iMTExMSI%3D",  # 1111
     #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rKz5Y2XIg%3D%3D",  # 河南
@@ -250,7 +481,7 @@ for result in results:
     if result:
         channel_name, channel_url = result.split(',')
         channels.append((channel_name, channel_url))
-with open("iptv.txt", 'w', encoding='utf-8') as file:
+with open("iptv.txt", 'a', encoding='utf-8') as file:           #打开文本以追加的形式写入行
     for result in results:
         file.write(result + "\n")
         print(result)
@@ -501,7 +732,7 @@ replacements = {
     	"CCTV10CCTV10": "CCTV10"
 }
 # 打开新文本文件准备写入替换后的内容
-with open('光迅源.txt', 'a', encoding='utf-8') as new_file:        #打开光迅源文本以追加的形式写入行
+with open('酒店源.txt', 'w', encoding='utf-8') as new_file:        #打开酒店源文本以追加的形式写入行
     for line in lines:
         # 去除行尾的换行符
         line = line.rstrip('\n')
@@ -546,10 +777,10 @@ replacements = {
         "": ""
 }
 # 打开原始文件读取内容，并写入新文件
-with open('光迅源.txt', 'r', encoding='utf-8') as file:
+with open('酒店源.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 # 创建新文件并写入替换后的内容
-with open('光迅源.txt', 'w', encoding='utf-8') as new_file:
+with open('酒店源.txt', 'w', encoding='utf-8') as new_file:
     for line in lines:
         for old, new in replacements.items():
             line = line.replace(old, new)
@@ -558,7 +789,7 @@ print("替换完成，新文件已保存。")
 ########################################################################################################################################################################################
 #################文本排序
 # 打开原始文件读取内容，并写入新文件
-with open('光迅源.txt', 'r', encoding='utf-8') as file:
+with open('酒店源.txt', 'r', encoding='utf-8') as file:
     lines = file.readlines()
 # 定义一个函数，用于提取每行的第一个数字
 def extract_first_number(line):
@@ -570,7 +801,7 @@ sorted_lines = sorted(lines, key=lambda x: (not 'CCTV' in x, extract_first_numbe
 # 将排序后的行写入新的utf-8编码的文本文件，文件名基于原文件名
 output_file_path = "sorted_" + os.path.basename(file_path)
 # 写入新文件
-with open('光迅源.txt', "w", encoding="utf-8") as file:
+with open('酒店源.txt', "w", encoding="utf-8") as file:
     for line in sorted_lines:
         file.write(line)
 print(f"文件已排序并保存为: {output_file_path}")
@@ -580,12 +811,12 @@ print(f"文件已排序并保存为: {output_file_path}")
 converter = OpenCC('t2s.json')#繁转简
 #converter = OpenCC('s2t.json')#简转繁
 # 打开txt文件
-with open('光迅源.txt', 'r', encoding='utf-8') as file:
+with open('酒店源.txt', 'r', encoding='utf-8') as file:
     traditional_text = file.read()
 # 进行繁体字转简体字的转换
 simplified_text = converter.convert(traditional_text)
 # 将转换后的简体字写入txt文件
-with open('光迅源.txt', 'w', encoding='utf-8') as file:
+with open('酒店源.txt', 'w', encoding='utf-8') as file:
     file.write(simplified_text)
 ########################################################################################################################################################################################
 ################################################################定义关键词分割规则
@@ -611,33 +842,33 @@ def check_and_write_file(input_file, output_file, keywords):
     else:
         print(f"文件已提取关键词并保存为: {output_file}")
 # 按类别提取关键词并写入文件
-check_and_write_file('光迅源.txt',  'a0.txt',  keywords="央视频道, 8K, 4K, 4k")
-check_and_write_file('光迅源.txt',  'a.txt',  keywords="央视频道, CCTV, 8K, 4K, 爱上4K, 纯享, 风云剧场, 怀旧剧场, 影迷, 高清电影, 动作电影, 第一剧场, 家庭影院, 影迷电影, 星光, 华语, 峨眉")
-check_and_write_file('光迅源.txt',  'a1.txt',  keywords="央视频道, 风云音乐, 音乐现场")
+check_and_write_file('酒店源.txt',  'a0.txt',  keywords="央视频道, 8K, 4K, 4k")
+check_and_write_file('酒店源.txt',  'a.txt',  keywords="央视频道, CCTV, 8K, 4K, 爱上4K, 纯享, 风云剧场, 怀旧剧场, 影迷, 高清电影, 动作电影, 第一剧场, 家庭影院, 影迷电影, 星光, 华语, 峨眉")
+check_and_write_file('酒店源.txt',  'a1.txt',  keywords="央视频道, 风云音乐, 音乐现场")
 
-check_and_write_file('光迅源.txt',  'b.txt',  keywords="卫视频道, 卫视, 凤凰， 星空")
+check_and_write_file('酒店源.txt',  'b.txt',  keywords="卫视频道, 卫视, 凤凰， 星空")
 
-check_and_write_file('光迅源.txt',  'c.txt',  keywords="影视频道, 爱情喜剧, 爱喜喜剧, 电影, 惊嫊悬疑, 东北热剧, 无名, 都市剧场, iHOT, 剧场, 欢笑剧场, 重温经典, 明星大片, 中国功夫, 军旅, 东北热剧, 中国功夫, 军旅剧场, 古装剧场, \
+check_and_write_file('酒店源.txt',  'c.txt',  keywords="影视频道, 爱情喜剧, 爱喜喜剧, 电影, 惊嫊悬疑, 东北热剧, 无名, 都市剧场, iHOT, 剧场, 欢笑剧场, 重温经典, 明星大片, 中国功夫, 军旅, 东北热剧, 中国功夫, 军旅剧场, 古装剧场, \
 家庭剧场, 惊悚悬疑, 欢乐剧场, 潮妈辣婆, 爱情喜剧, 精品大剧, 超级影视, 超级电影, 黑莓动画, 黑莓电影, 海外剧场, 精彩影视, 无名影视, 潮婆辣妈, 超级剧, 热播精选")
-check_and_write_file('光迅源.txt',  'c1.txt',  keywords="影视频道, 求索动物, 求索, 求索科学, 求索记录, 爱谍战, 爱动漫, 爱科幻, 爱青春, 爱自然, 爱科学, 爱浪漫, 爱历史, 爱旅行, 爱奇谈, 爱怀旧, 爱赛车, 爱都市, 爱体育, 爱经典, \
+check_and_write_file('酒店源.txt',  'c1.txt',  keywords="影视频道, 求索动物, 求索, 求索科学, 求索记录, 爱谍战, 爱动漫, 爱科幻, 爱青春, 爱自然, 爱科学, 爱浪漫, 爱历史, 爱旅行, 爱奇谈, 爱怀旧, 爱赛车, 爱都市, 爱体育, 爱经典, \
 爱玩具, 爱喜剧, 爱悬疑, 爱幼教, 爱院线")
-check_and_write_file('光迅源.txt',  'c光迅源.txt',  keywords="影视频道, 军事评论, 农业致富, 哒啵赛事, 怡伴健康, 武博世界, 超级综艺, 哒啵, HOT, 炫舞未来, 精品体育, 精品萌宠, 精品记录, 超级体育, 金牌, 武术世界, 精品纪录")
+check_and_write_file('酒店源.txt',  'c酒店源.txt',  keywords="影视频道, 军事评论, 农业致富, 哒啵赛事, 怡伴健康, 武博世界, 超级综艺, 哒啵, HOT, 炫舞未来, 精品体育, 精品萌宠, 精品记录, 超级体育, 金牌, 武术世界, 精品纪录")
 
-check_and_write_file('光迅源.txt',  'd.txt',  keywords="少儿频道, 少儿, 卡通, 动漫, 宝贝, 哈哈")
+check_and_write_file('酒店源.txt',  'd.txt',  keywords="少儿频道, 少儿, 卡通, 动漫, 宝贝, 哈哈")
 
-check_and_write_file('光迅源.txt',  'e.txt',  keywords="港澳频道, TVB, 珠江台, 澳门, 龙华, 广场舞, 动物杂技, 民视, 中视, 华视, AXN, MOMO, 采昌, 耀才, 靖天, 镜新闻, 靖洋, 莲花, 年代, 爱尔达, 好莱坞, 华丽, 非凡, 公视, 寰宇, 无线, EVEN, MoMo, 爆谷, 面包, momo, 唐人, \
+check_and_write_file('酒店源.txt',  'e.txt',  keywords="港澳频道, TVB, 珠江台, 澳门, 龙华, 广场舞, 动物杂技, 民视, 中视, 华视, AXN, MOMO, 采昌, 耀才, 靖天, 镜新闻, 靖洋, 莲花, 年代, 爱尔达, 好莱坞, 华丽, 非凡, 公视, 寰宇, 无线, EVEN, MoMo, 爆谷, 面包, momo, 唐人, \
 中华小, 三立, CNA, FOX, RTHK, Movie, 八大, 中天, 中视, 东森, 凤凰, 光迅, 天映, 美亚, 环球, 翡翠, 亚洲, 大爱, 大愛, 明珠, 半岛, AMC, 龙祥, 台视, 1905, 纬来, 神话, 经典都市, 视界, 番薯, 私人, 光迅, TVB, 凤凰, 半岛, 星光视界, \
 番薯, 大愛, 新加坡, 星河, 明珠, 环球, 翡翠台")
 
-check_and_write_file('光迅源.txt',  'f.txt',  keywords="省市频道, 湖北, 武汉, 河北, 广东, 河南, 广西, 南宁, 福建, 辽宁")
-check_and_write_file('光迅源.txt',  'f1.txt',  keywords="省市频道, 公共, 综合, 影视, 新闻")
+check_and_write_file('酒店源.txt',  'f.txt',  keywords="省市频道, 湖北, 武汉, 河北, 广东, 河南, 广西, 南宁, 福建, 辽宁")
+check_and_write_file('酒店源.txt',  'f1.txt',  keywords="省市频道, 公共, 综合, 影视, 新闻")
 
-check_and_write_file('光迅源.txt',  'o1.txt',  keywords="其他频道, 新闻, 综合, 文艺, 电视, 公共, 科教, 教育, 民生, 轮播, 套, 法制, 文化, 经济, 生活")
-check_and_write_file('光迅源.txt',  'o.txt',  keywords="其他频道, , ")
+check_and_write_file('酒店源.txt',  'o1.txt',  keywords="其他频道, 新闻, 综合, 文艺, 电视, 公共, 科教, 教育, 民生, 轮播, 套, 法制, 文化, 经济, 生活")
+check_and_write_file('酒店源.txt',  'o.txt',  keywords="其他频道, , ")
 ###############################################################################################################################################################################################################################
 ##############################################################对生成的文件进行合并
 file_contents = []
-file_paths = ["e.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "c1.txt", "c光迅源.txt", "d.txt", "f.txt", "f1.txt", "o1.txt", "o.txt"]  # 替换为实际的文件路径列表
+file_paths = ["e.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "c1.txt", "c酒店源.txt", "d.txt", "f.txt", "f1.txt", "o1.txt", "o.txt"]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     if os.path.exists(file_path):
         with open(file_path, 'r', encoding="utf-8") as file:
@@ -663,7 +894,7 @@ for line in lines:
   unique_lines.append(line)
   seen_lines.add(line)
 # 将唯一的行写入新的文档 
-with open('光迅源.txt', 'w', encoding="utf-8") as file:
+with open('酒店源.txt', 'w', encoding="utf-8") as file:
  file.writelines(unique_lines)
 ################################################################################################任务结束，删除不必要的过程文件
 files_to_remove = ['去重.txt', "2.txt", "iptv.txt", "e.txt", "a0.txt", "a.txt", "a1.txt", "b.txt", "c.txt", "c1.txt", "c2.txt", "d.txt", "f.txt", "f1.txt", "o1.txt", "o.txt", "检测结果.txt"]
@@ -672,4 +903,4 @@ for file in files_to_remove:
         os.remove(file)
     else:              # 如果文件不存在，则提示异常并打印提示信息
         print(f"文件 {file} 不存在，跳过删除。")
-print("任务运行完毕，光迅源频道列表可查看文件夹内txt文件！")
+print("任务运行完毕，酒店源频道列表可查看文件夹内txt文件！")
