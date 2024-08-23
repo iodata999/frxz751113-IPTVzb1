ecbad33fc5a0f338151a8eaa04a63cceb61001b3
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-08-23 13:00:19 +0800

    Delete 1.php

--- 1.php
+++
@@ -1,8 +0,0 @@
-每天6点30分自动更新,#genre#
-重温经典,http://cg12.hunancatv.cn:9090/live/CWJD.m3u8?deviceid=866265039837531&sid=Yp6brv0MWYukAl4oJr9yrw%3D%3D&nonce=Ri1TgJg9dlAK&acl=0111&hlsSessionID=1503359536318867014&srchost=172.16.22.158:8060&gid=OHehEP1KV9FqguWiPXrX6Q%3D%3D
-倩女幽魂妖魔道,http://jsmov2.a.yximgs.com/bs3/video-hls/5232901391302199073_hlsb.m3u8
-倩女幽魂人间道,http://jsmov2.a.yximgs.com/bs3/video-hls/5250634317044855747_hlsb.m3u8
-倩女幽魂道道道,http://jsmov2.a.yximgs.com/bs3/video-hls/5216294367991385255_hlsb.m3u8
-河南尉氏,http://tvpull.dxhmt.cn:9081/tv/10223-1.m3u8
-河南梨园,http://dsm.ikun.hn.cn:9981/stream/channelid/1727631217?profile=pass
-河南梨园,http://117.72.68.25:9930/hn.php?id=hnly#http://ojbk.02200059.xyz:4022/udp/226.1.30.96:6000
