a4c8e96b829b616082d40306826ae6de39c28714
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-09-12 21:42:21 +0800

    Update m3u加运行时间.py

--- 组播py/m3u加运行时间.py
+++ 组播py/m3u加运行时间.py
@@ -1,30 +1,53 @@
+# uncompyle6 version 3.9.2
+
+# Python bytecode version base 3.8.0 (3413)
+
+# Decompiled from: Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)]
+
+# Embedded file name: 1.py
+
+import time, os, re, base64, datetime, requests, threading
+
+from queue import Queue
+
+from datetime import datetime
+
+
+
 def txt_to_m3u(input_file, output_file):
-    # 读取txt文件内容
-    with open(input_file, 'r', encoding='utf-8') as f:
+
+    with open(input_file, "r", encoding="utf-8") as f:
+
         lines = f.readlines()
 
-    # 打开m3u文件并写入内容
-    with open(output_file, 'w', encoding='utf-8') as f:
+    with open(output_file, "w", encoding="utf-8") as f:
+
         f.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"\n')
-        # 初始化genre变量
-        genre = ''
 
-        # 遍历txt文件内容
+        genre = ""
+
         for line in lines:
-            line = line.strip()
-            if "," in line:  # 防止文件里面缺失“,”号报错
-                # if line:
-                # 检查是否是genre行
-                channel_name, channel_url = line.split(',', 1)
-                if channel_url == '#genre#':
-                    genre = channel_name
-                    print(genre)
-                else:
-                    # 将频道信息写入m3u文件
-                    f.write(f'#EXTINF:-1 tvg-id="{channel_name}" tvg-name="{channel_name}" tvg-logo="https://live.fanmingming.com/tv/{channel_name}.png" group-title="{genre}",{channel_name}\n')
-                    f.write(f'{channel_url}\n')
 
-if __name__ == "__main__":
-    input_file = "ceshi.txt"
-    output_file = "ceshi.m3u"
-    txt_to_m3u(input_file, output_file)
+            line = line.strip()
+
+            if "," in line:
+
+                channel_name, channel_url = line.split(",", 1)
+
+                if channel_url == "#genre#":
+
+                    genre = channel_name
+
+                    print(genre)
+
+                else:
+
+                    f.write(f'#EXTINF:-1 tvg-logo="https://raw.githubusercontent.com/linitfor/epg/main/logo/{channel_name}.png" group-title="{genre}",{channel_name}\n')
+
+                    f.write(f"{channel_url}\n")
+
+
+
+
+
+txt_to_m3u("ceshi.txt", "ceshi.m3u")
