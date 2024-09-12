35f7c067552a2a9b9f8717dedb0bb320018d1cef
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-09-12 17:11:12 +0800

    Update m3u加运行时间.py

--- 组播py/m3u加运行时间.py
+++ 组播py/m3u加运行时间.py
@@ -22,4 +22,4 @@ def add_run_time_to_m3u(iptv_list):
 
 if __name__ == "__main__":
     m3u_file_path = "iptv_list.m3u"
-    add_run_time_to_m3u(m3u_file_path)
+    add_run_time_to_m3u(iptv_list.m3u)
