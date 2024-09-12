d300625305e3965671bfa27c746188e1651232bd
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-09-12 19:24:16 +0800

    Update txt加运行时间2.py

--- 组播py/txt加运行时间2.py
+++ 组播py/txt加运行时间2.py
@@ -2,17 +2,17 @@ import os
 import glob
 
 
-def merge_txt_files(iptv_list, runtime):
+def merge_txt_files(runtime, iptv_list):
     try:
-        with open(iptv_list, 'a') as f1:
-            with open(runtime, 'r') as f2:
+        with open(runtime, 'a') as f1:
+            with open(iptv_list, 'r') as f2:
                 for line in f2:
                     f1.write(line)
-        print(f"已成功将 {runtime} 合并到 {iptv_list}")
+        print(f"已成功将 {iptv_list} 合并到 {runtime}")
     except FileNotFoundError as e:
         print(f"错误：{e}")
 
 
 if __name__ == "__main__":
-    merge_txt_files('iptv_list.txt', 'runtime.txt')
+    merge_txt_files('runtime.txt', 'iptv_list.txt')
     
