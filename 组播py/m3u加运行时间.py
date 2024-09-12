78d05689447371f75981c0cffda854a415cb88fe
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-09-12 21:22:35 +0800

    Update m3u加运行时间.py

--- 组播py/m3u加运行时间.py
+++ 组播py/m3u加运行时间.py
@@ -1,19 +1,30 @@
-def txt_to_m3u(txt_file_path, m3u_file_path):
-    try:
-        with open(txt_file_path, 'r') as txt_f:
-            lines = txt_f.readlines()
-            with open(m3u_file_path, 'w') as m3u_f:
-                m3u_f.write('#EXTM3U\n')
-                for line in lines:
-                    line = line.strip()
-                    if line:
-                        m3u_f.write(f'#EXTINF:-1,\n{line}\n')
-        print(f"已成功将 {txt_file_path} 转换为 {m3u_file_path}")
-    except FileNotFoundError:
-        print(f"文件 {txt_file_path} 或 {m3u_file_path} 未找到。")
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
 
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"\n')
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            if "," in line:  # 防止文件里面缺失“,”号报错
+                # if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 tvg-id="{channel_name}" tvg-name="{channel_name}" tvg-logo="https://live.fanmingming.com/tv/{channel_name}.png" group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
 
 if __name__ == "__main__":
-    txt_file_path = "ceshi.txt"
-    m3u_file_path = "ceshi.m3u"
+    input_file = "ceshi.txt"
+    output_file = "ceshi.m3u"
     txt_to_m3u(txt_file_path, m3u_file_path)
