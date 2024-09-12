de47a2521c49d777479cb20fa8aa6f4df61ad05d
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-09-12 11:18:26 +0800

    Update 文件合并.py

--- 组播py/文件合并.py
+++ 组播py/文件合并.py
@@ -2,13 +2,13 @@ import os
 import glob
 
 
-def merge_txt_files(runtime.txt, iptv_list.txt):
+def merge_txt_files(runtime, iptv_list):
     try:
-        with open(runtime.txt, 'a') as f1:
-            with open(iptv_list.txt, 'r') as f2:
+        with open(runtime, 'a') as f1:
+            with open(iptv_list, 'r') as f2:
                 for line in f2:
                     f1.write(line)
-        print(f"已成功将 {iptv_list.txt} 合并到 {runtime.txt}")
+        print(f"已成功将 {iptv_list} 合并到 {runtime}")
     except FileNotFoundError as e:
         print(f"错误：{e}")
 
