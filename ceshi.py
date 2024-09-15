6a7f46c2af60dd32ee0f41a537154a3aef87d5b3
aichidigua <151888742+mengxianshengaaa@users.noreply.github.com>
2024-09-15 14:42:05 +0800

    Update ceshi.py

--- ceshi.py
+++ ceshi.py
@@ -3,10 +3,11 @@ import time
 
 
 def dynamic_file_naming():
-    # 获取当前时间戳
-    timestamp = int(time.time())
+    # 获取当前时间的月日信息
+    now = time.localtime()
+    month_day = time.strftime('%m%d', now)
     file_extension = '.txt'
-    new_file_name = f'file_{timestamp}{file_extension}'
+    new_file_name = f'file_{month_day}{file_extension}'
 
     # 假设之前有一个旧的动态命名文件，这里简单查找以'file_'开头的.txt文件作为旧文件
     old_file = None
@@ -17,14 +18,19 @@ def dynamic_file_naming():
 
     if old_file:
         os.rename(old_file, new_file_name)
+        return new_file_name
+        
+    #else:
+        #return None #这两行代码找不到文件跳过
+
+    
     else:
-        # 如果没有旧文件，直接创建新文件
-        with open(new_file_name, 'w') as f:
-            pass
-
-    return new_file_name
-
+         如果没有旧文件，直接创建新文件
+       with open(new_file_name, 'w') as f:
+         pass
+      return new_file_name# 四行代码找不到文件创建新文件
 
 if __name__ == '__main__':
     new_name = dynamic_file_naming()
-    print(f'新文件名为: {new_name}')
+    if new_name:
+        print(f'新文件名为: {new_name}')
